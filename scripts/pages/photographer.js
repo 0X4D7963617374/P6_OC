2df4ade78c74d1b612e9f115aee69df4808ca8ec
0X4D7963617374 <olivier@helioscast.com>
2024-06-06 22:30:16 +0200

    photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -12,90 +12,217 @@ async function fetchJson(url)
 async function getPhoto(id)
 {
     const data = await fetchJson(dataUrl);
-    const photographer = data.photographers.find(photographer => photographer.id == id);
-    return photographer;
+    let i = 0;
+    const photographers = data.photographers;
+
+    while (i < photographers.length)
+    {
+        if (photographers[i].id == id)
+            return photographers[i];
+        i++;
+    }
+    return null;
 }
 
 async function getMedia(id)
 {
     const data = await fetchJson(dataUrl);
-    const media = data.media.filter(media => media.photographerId == id);
+    let media = [];
+    let i = 0;
+
+    while (i < data.media.length)
+    {
+        if (data.media[i].photographerId == id)
+            media.push(data.media[i]);
+        i++;
+    }
     return media;
 }
 
-function createPhotographerDetailsHtml(photographer)
+function getPhotographerCardDOM(name, city, country, tagline, price)
 {
-    const picture = `assets/photographers/${photographer.portrait}`;
-    return `
-        <img src="${picture}" alt="${photographer.name}, ${photographer.tagline}">
-        <h2>${photographer.name}</h2>
-        <p>${photographer.city}, ${photographer.country}</p>
-        <p>${photographer.tagline}</p>
-        <p>${photographer.price}€/heure</p>`;
+    const article = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const cityCountry = document.createElement('p');
+    const tag = document.createElement('p');
+    const pricePerHour = document.createElement('p');
+
+    article.classList.add("photographer-detail-photographer");
+    h2.textContent = name;
+    cityCountry.textContent = `${city}, ${country}`;
+    tag.textContent = tagline;
+    pricePerHour.textContent = `${price}€/heure`;
+
+    article.appendChild(h2);
+    article.appendChild(cityCountry);
+    article.appendChild(tag);
+    article.appendChild(pricePerHour);
+
+    return article;
+}
+
+function photographerFactory(data)
+{
+    const { name, city, country, tagline, price, portrait } = data;
+    return { getPhotographerCardDOM };
+}
+
+function createMediaPath(data, photographerFirstName)
+{
+    const { image, video } = data;
+    return image ? `assets/images/${photographerFirstName}/${image}` : `assets/images/${photographerFirstName}/${video}`;
+}
+
+function getMediaDOM(data, mediaPath)
+{
+    const { image, video, title, likes } = data;
+    const article = document.createElement('article');
+    article.classList.add("media-card-photographer");
+
+    let mediaElement;
+    if (image)
+    {
+        mediaElement = document.createElement('img');
+        mediaElement.setAttribute('src', mediaPath);
+        mediaElement.setAttribute('alt', title);
+    }
+    else if (video)
+    {
+        mediaElement = document.createElement('video');
+        mediaElement.setAttribute('controls', '');
+        const source = document.createElement('source');
+        source.setAttribute('src', mediaPath);
+        source.setAttribute('type', 'video/mp4');
+        mediaElement.appendChild(source);
+    }
+
+    const info = document.createElement('div');
+    const mediaTitle = document.createElement('h3');
+    const mediaLikes = document.createElement('div');
+    const likesCount = document.createElement('span');
+    const heartIcon = document.createElement('span');
+
+    info.classList.add('info');
+    mediaTitle.textContent = title;
+    mediaLikes.classList.add('likes');
+    likesCount.textContent = likes;
+    heartIcon.classList.add('heart', 'fas', 'fa-heart');
+
+    heartIcon.addEventListener('click', () =>
+    {
+        if (heartIcon.classList.contains('liked'))
+        {
+            likesCount.textContent = parseInt(likesCount.textContent) - 1;
+            heartIcon.classList.remove('liked');
+        } else
+        {
+            likesCount.textContent = parseInt(likesCount.textContent) + 1;
+            heartIcon.classList.add('liked');
+        }
+    });
+
+    mediaLikes.appendChild(likesCount);
+    mediaLikes.appendChild(heartIcon);
+    info.appendChild(mediaTitle);
+    info.appendChild(mediaLikes);
+    article.appendChild(mediaElement);
+    article.appendChild(info);
+
+    return article;
+}
+
+function mediaFactory(data, photographerFirstName)
+{
+    const mediaPath = createMediaPath(data, photographerFirstName);
+    return { getMediaDOM: () => getMediaDOM(data, mediaPath) };
+}
+
+function sortMedia(media, criterion)
+{
+    let sortedMedia;
+    switch (criterion)
+    {
+        case 'popularity':
+            sortedMedia = media.sort((a, b) => b.likes - a.likes);
+            break;
+        case 'date':
+            sortedMedia = media.sort((a, b) => new Date(b.date) - new Date(a.date));
+            break;
+        case 'title':
+            sortedMedia = media.sort((a, b) => a.title.localeCompare(b.title));
+            break;
+        default:
+            sortedMedia = media;
+    }
+    return sortedMedia;
 }
 
 async function displayPhotographerDetails(photographer)
 {
-    const photographerSection = document.querySelector(".photographer_details");
-    const article = document.createElement('article');
-    article.classList.add("photographer-detail");
-    article.innerHTML = createPhotographerDetailsHtml(photographer);
-    photographerSection.appendChild(article);
-}
+    const photographerSection = document.querySelector(".photographer-details-photographer");
+    const photographerCardDOM = getPhotographerCardDOM(photographer.name, photographer.city, photographer.country, photographer.tagline, photographer.price);
+    photographerSection.appendChild(photographerCardDOM);
 
-function createMediaCardHtml(item, photographerFirstName)
-{
-    let mediaContent = '';
-    const mediaImagePath = item.image ? `assets/images/${photographerFirstName}/${item.image}` : '';
-    const mediaVideoPath = item.video ? `assets/images/${photographerFirstName}/${item.video}` : '';
-
-    if (item.image)
-        mediaContent = `<img src="${mediaImagePath}" alt="${item.title}">`;
-    else if (item.video)
-        mediaContent = `
-            <video controls>
-                <source src="${mediaVideoPath}" type="video/mp4">
-            </video>`;
-    return `
-        ${mediaContent}
-        <h3>${item.title}</h3>
-        <p>${item.likes} ♡</p>
-        <p>${item.date}</p>`;
+    const photographerPortrait = document.querySelector('.photographer-portrait-photographer');
+    photographerPortrait.setAttribute('src', `assets/photographers/${photographer.portrait}`);
+    photographerPortrait.setAttribute('alt', photographer.name);
 }
 
 async function displayMedia(media, photographerFirstName)
 {
-    const mediaSection = document.querySelector(".media_section");
-    const mediaCards = [];
+    const mediaSection = document.querySelector(".media-section-photographer");
+    mediaSection.innerHTML = '';
+    let mediaCards = [];
     let i = 0;
-    let item, mediaCard;
-
     while (i < media.length)
     {
-        item = media[i++];
-        mediaCard = document.createElement('article');
-        mediaCard.classList.add("media-card");
-        mediaCard.innerHTML = createMediaCardHtml(item, photographerFirstName);
+        let item = media[i];
+        let mediaCard = mediaFactory(item, photographerFirstName).getMediaDOM();
         mediaCards.push(mediaCard);
+        i++;
     }
 
     i = 0;
     while (i < mediaCards.length)
-        mediaSection.appendChild(mediaCards[i++]);  
+        mediaSection.appendChild(mediaCards[i++]);
 
 }
 
 async function init()
 {
     const photographer = await getPhoto(photographerId);
-    const media = photographer ? await getMedia(photographerId) : [];
+    let media = [];
+    if (photographer)
+        media = await getMedia(photographerId);
     const photographerFirstName = photographer ? photographer.name.split(" ")[0].replace(/-/, ' ') : '';
-
     if (photographer)
         displayPhotographerDetails(photographer);
     if (media.length > 0)
         displayMedia(media, photographerFirstName);
 
+    const selectElement = document.getElementById('sort-photographer');
+    selectElement.addEventListener('change', (event) =>
+    {
+        const sortedMedia = sortMedia(media, event.target.value);
+        displayMedia(sortedMedia, photographerFirstName);
+    });
 }
 
+document.addEventListener('DOMContentLoaded', () =>
+{
+    const selectElement = document.getElementById('sort-photographer');
+    const sortContainer = document.querySelector('.sort-section-photographer');
+
+    selectElement.addEventListener('click', () =>
+    {
+        sortContainer.classList.toggle('open');
+    });
+
+    selectElement.addEventListener('blur', () =>
+    {
+        sortContainer.classList.remove('open');
+    });
+});
+
 init();
+
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -3,24 +3,64 @@ function photographerTemplate(data)
     const { name, city, country, tagline, price, portrait, id } = data;
     const picture = `assets/photographers/${portrait}`;
 
-    const getUserCardDOM = createPhotographerCard.bind(null, name, picture, city, country, tagline, price, id);
-
-    return { getUserCardDOM };
+    return {
+        name,
+        picture,
+        getUserCardDOM: () => createPhotographerCard(name, picture, city, country, tagline, price, id)
+    };
 }
 
 function createPhotographerCard(name, picture, city, country, tagline, price, id)
 {
     const article = document.createElement('article');
+    const link = document.createElement('a');
+    const imageContainer = document.createElement('div');
+    const svg = `
+        <svg width="300" height="300" viewBox="0 0 150 150" class="photographer-svg">
+            <defs>
+                <radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+                    <stop offset="0%" style="stop-color:rgba(255, 255, 255, 0.0);stop-opacity:0" />
+                    <stop offset="100%" style="stop-color:rgba(255, 255, 255, 1);stop-opacity:1" />
+                </radialGradient>
+            </defs>
+            <circle cx="75" cy="75" r="75" fill="rgba(255, 255, 255, 0.0)"/>
+            <circle cx="75" cy="75" r="70" fill="none" stroke="url(#grad1)" stroke-width="20"/>
+        </svg>
+    `;
+    const img = document.createElement('img');
+    const h2 = document.createElement('h2');
+    const cityCountry = document.createElement('p');
+    const tag = document.createElement('p');
+    const pricePerHour = document.createElement('p');
+
     article.classList.add("photographer-card");
 
-    article.innerHTML =
-    `<a href="photographer.html?id=${id}">
-        <img src="${picture}" alt="${name}, ${tagline}">
-        <h2>${name}</h2>
-        <p>${city}, ${country}</p>
-        <p>${tagline}</p>
-        <p>${price}€/heure</p>
-    </a>`;
+    link.href = `photographer.html?id=${id}`;
+    link.classList.add("photographer-link");
+
+    imageContainer.classList.add("image-container");
+
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", `${name}, ${tagline}`);
+    img.classList.add("photographer-portrait");
+
+    imageContainer.innerHTML = svg;
+    imageContainer.appendChild(img);
+
+    h2.textContent = name;
+    h2.classList.add("photographer-name");
+
+    cityCountry.textContent = `${city}, ${country}`;
+    cityCountry.classList.add("photographer-location");
+
+    tag.textContent = tagline;
+    tag.classList.add("photographer-tagline");
+
+    pricePerHour.textContent = `${price}€/heure`;
+    pricePerHour.classList.add("photographer-price");
+
+    link.append(imageContainer, h2, cityCountry, tag, pricePerHour);
+    article.appendChild(link);
 
     return article;
 }
